7b14ecbe2dda4241ec46dcc03e6a90c61f41a635
Sainfuw <sainfuw@aol.com>
2023-06-24 06:42:22 -0400

    clock working

---
+++ main.js
@@ -0,0 +1,46 @@
+function getTotalMinutes(ct) {
+	const hours = ct.getHours();
+	const minutes = ct.getMinutes();
+
+	return hours * 60 + minutes;
+}
+
+function stringDate(time) {
+	return time.toString().length === 1 ? `0${time}` : time;
+}
+
+function changeAngle() {
+	const vw = Math.max(
+		document.documentElement.clientWidth || 0,
+		window.innerWidth || 0
+	);
+	const r = (vw - 44) / 2;
+
+	const dayPercent = getTotalMinutes(currentTime) / 1440;
+	const dayAngle = dayPercent * 90 + 45;
+
+	const coordX = r * Math.sin((Math.PI * 2 * dayAngle) / 360);
+	const coordY = r * Math.cos((Math.PI * 2 * dayAngle) / 360);
+	const right = (coordY + r).toFixed(0);
+	const bottom = (coordX - r / 1.67).toFixed(0);
+
+	document.body.style.setProperty('--coordX', right + 'px');
+	document.body.style.setProperty('--coordY', bottom + 'px');
+
+	document.querySelector('.time-container h3').innerHTML = `${stringDate(
+		currentTime.getHours()
+	)}:${stringDate(currentTime.getMinutes())}`;
+}
+
+const currentTime = new Date();
+const currentDate = new Intl.DateTimeFormat('es-CL', {
+	weekday: 'long',
+	year: 'numeric',
+	month: 'long',
+	day: 'numeric',
+});
+
+document.querySelector('header h1').innerHTML = currentDate.format(currentTime);
+
+changeAngle();
+window.addEventListener('resize', () => changeAngle());
